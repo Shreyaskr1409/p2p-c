b7233bb12645a8f39dff6030dfcb34b6d2505811
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-08-28 23:17:10 +0530

    Kind of made a mock p2p which updates known hosts for clients and server simultaneously

---
+++ README.md
@@ -0,0 +1,6 @@
+# P2P in C
+
+This repository is for practicing how to  build a peer to peer network in C so that
+I can use the knowledge which I gain somewhere else as well.
+
+I am heavily relying on an external library for learning purposes though I might use something else later on: https://github.com/ericomeehan/libeom
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include "libeom/Networking/Nodes/Client.h"
-#include "libeom/Networking/Nodes/Server.h"
 #include <libeom/libeom.h>
 #include <bits/pthreadtypes.h>
 #include <netinet/in.h>
@@ -12,34 +10,53 @@
 
 void * server_function(void *arg) {
     printf("Server running.\n");
+
+    struct LinkedList *known_hosts = arg;
     struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 5858, 20);
     struct sockaddr *addr = (struct sockaddr *)&server.address;
     socklen_t addr_len = (socklen_t)sizeof(server.address);
+
     while (1) {
         int fd = accept(server.socket, addr, &addr_len);
         char request[255];
         memset(request, 0, 255);
         read(fd, request, 255);
-        printf("\t\t\t%s says: %s\n", inet_ntoa(server.address.sin_addr), request);
+        char *client_address = inet_ntoa(server.address.sin_addr);
+        printf("\t\t\t%s says: %s\n", client_address, request);
         close(fd);
+
+        short found = 0;
+        for (int i = 0; i < known_hosts->length && !found; i++) {
+            if (strcmp(client_address, known_hosts->retrieve(known_hosts, i)) == 0) {
+                found = 1;
+            }
+        }
+        if (!found) {
+            known_hosts->insert(known_hosts, known_hosts->length, client_address, sizeof(client_address));
+        }
     }
     return NULL;
 }
 
-void client_function(char *request) {
+void client_function(char *request, struct LinkedList *known_hosts) {
     struct Client client = client_constructor(AF_INET, SOCK_STREAM, 0, 5858, INADDR_ANY);
-    client.request(&client, "127.0.0.1", request, 255);
+    for (int i = 0; i < known_hosts->length; i++) {
+        client.request(&client, known_hosts->retrieve(known_hosts, i), request, 255);
+    }
 }
 
 int main() {
+    struct LinkedList known_hosts = linked_list_constructor();
+    known_hosts.insert(&known_hosts, 0, "127.0.0.1", 10);
+
     printf("Starting server...\n");
     pthread_t server_thread;
-    pthread_create(&server_thread, NULL, server_function, NULL);
+    pthread_create(&server_thread, NULL, server_function, &known_hosts);
 
     while (1) {
         char request[255];
         memset(request, 0, 255);
         fgets(request, 255, stdin);
-        client_function(request);
+        client_function(request, &known_hosts);
     }
 }
