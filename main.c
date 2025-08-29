3c2ec36dfe042c6734eb7946e27234248aeb0d09
Shreyas Kumar <shreyaskr.0914@gmail.com>
2025-08-29 23:48:29 +0530

    Made the server multithread but it still has some race condition related issues

--- main.c
+++ main.c
@@ -8,38 +8,63 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+struct ServerLoopArgument {
+    int fd;
+    struct Server *server;
+    struct LinkedList *known_hosts;
+};
+
+void * server_loop(void *arg) {
+    struct ServerLoopArgument *sla = arg;
+
+    char request[255];
+    memset(request, 0, 255);
+    read(sla->fd, request, 255);
+    char *client_address = inet_ntoa(sla->server->address.sin_addr);
+
+    printf("\t\t\t%s says: %s\n", client_address, request);
+    close(sla->fd);
+
+    short found = 0;
+    for (int i = 0; i < sla->known_hosts->length && !found; i++) {
+        if (strcmp(client_address, sla->known_hosts->retrieve(sla->known_hosts, i)) == 0) {
+            found = 1;
+        }
+    }
+    if (!found) {
+        sla->known_hosts->insert(sla->known_hosts, sla->known_hosts->length, client_address, sizeof(client_address));
+    }
+
+    return NULL;
+}
+
 void * server_function(void *arg) {
     printf("Server running.\n");
 
-    struct LinkedList *known_hosts = arg;
     struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 5858, 20);
     struct sockaddr *addr = (struct sockaddr *)&server.address;
     socklen_t addr_len = (socklen_t)sizeof(server.address);
 
+    struct ThreadPool thread_pool = thread_pool_constructor(2);
+    printf("Thread Pool allocated.\n");
     while (1) {
-        int fd = accept(server.socket, addr, &addr_len);
-        char request[255];
-        memset(request, 0, 255);
-        read(fd, request, 255);
-        char *client_address = inet_ntoa(server.address.sin_addr);
-        printf("\t\t\t%s says: %s\n", client_address, request);
-        close(fd);
+        printf("running\n");
+        struct ServerLoopArgument loop_arg;
 
-        short found = 0;
-        for (int i = 0; i < known_hosts->length && !found; i++) {
-            if (strcmp(client_address, known_hosts->retrieve(known_hosts, i)) == 0) {
-                found = 1;
-            }
-        }
-        if (!found) {
-            known_hosts->insert(known_hosts, known_hosts->length, client_address, sizeof(client_address));
-        }
+        loop_arg.fd = accept(server.socket, addr, &addr_len);
+        loop_arg.server = &server;
+        loop_arg.known_hosts = arg;
+
+        struct ThreadJob server_job = thread_job_constructor(server_loop, &loop_arg);
+        thread_pool.add_work(&thread_pool, server_job);
     }
+
     return NULL;
 }
 
 void client_function(char *request, struct LinkedList *known_hosts) {
     struct Client client = client_constructor(AF_INET, SOCK_STREAM, 0, 5858, INADDR_ANY);
+    printf("Client sent the request as: %s\n", request);
     for (int i = 0; i < known_hosts->length; i++) {
         client.request(&client, known_hosts->retrieve(known_hosts, i), request, 255);
     }
